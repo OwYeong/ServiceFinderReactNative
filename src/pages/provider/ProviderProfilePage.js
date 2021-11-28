30b8aedd39f8799d57ef29bb2704a3884d66821f
OwYeong <m.y.owyeong98@gmail.com>
2021-11-28 19:04:28 +0800

    #115 fixed the not scrollable bug in review tab

--- src/pages/provider/ProviderProfilePage.js
+++ src/pages/provider/ProviderProfilePage.js
@@ -323,6 +323,7 @@ const ProviderProfilePage = () => {
                                                 <ReviewDisplayComponent
                                                     dataList={reviewList}
                                                     averageRating={providerInfo?.averageRatings}
+                                                    isScrollEnabled={isPhotoListingScrollable}
                                                     starStats={providerInfo?.starStats}
                                                 />
                                             );
